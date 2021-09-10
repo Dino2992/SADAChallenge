1c117dc78d54e8992a7c4bb9b2785e124d69ef3e
Dino2992 <90434608+Dino2992@users.noreply.github.com>
2021-09-10 01:33:29 -0500

    Add files via upload

---
+++ Sada Challenge 1.py
@@ -0,0 +1,44 @@
+import csv
+import json
+
+rows_to_remove = []
+rows_to_keep = []
+# opens the csv file
+with open('data.csv') as data:
+    data_file = csv.DictReader(data)
+    line_num = 1
+    # creating new dic for csv file
+    fields = ['Year','Rank', 'Company', 'Revenue (in millions)', 'Profit (in millions)']
+    for row in data_file:
+        line_num += 1
+        try:
+            row['Profit (in millions)'] = float(row['Profit (in millions)'])
+            rows_to_keep.append(row)
+        except ValueError:
+            row['Profit (in millions)'] = str(row['Profit (in millions)'])
+            rows_to_remove.append(row)
+
+print()
+print(line_num)
+print()
+print(len(rows_to_keep))
+print()
+
+with open ('data2.json', 'w') as new_json:
+    json.dump(rows_to_keep, new_json)
+
+with open('data2.json') as j:
+    data2 = json.load(j)
+
+sorted_data2 = sorted(data2, key=lambda x: x['Profit (in millions)'], reverse=True)
+
+for row in range(20):
+    print(sorted_data2[row])
+
+
+
+
+
+
+
+
